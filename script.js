901c4b4c7178373d5cb6357cbc29448347224738
Øystein Håberg <75746434+Islandstone89@users.noreply.github.com>
2023-12-07 23:08:20 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+        
+        let button1 = document.getElementById("btn-1");
+        button1.addEventListener("click", () => {
+            let paragraph1 = document.getElementById("first-paragraph");
+            paragraph1.style.color = "red";
+        })
+
+        let button2 = document.getElementById("btn-2");
+        button2.addEventListener("click", () => {
+            let paragraph2 = document.getElementById("second-paragraph");
+            paragraph2.style.color = "green";
+        })
+
+        let button3 = document.getElementById("btn-3");
+        button3.addEventListener("click", () => {
+            let paragraph3 = document.getElementById("third-paragraph");
+            paragraph3.style.color = "blue";
+        })
+
+   
